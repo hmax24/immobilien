c1c7c04084b23bb0da2473dcd9d4106bf66cb431
Oleksii Maksymovych <al.v.maximovich@gmail.com>
2026-01-21 10:58:08 +0200

    init

---
+++ src/app/career/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Career() {
+  return (
+    <section>
+      <h2>Career</h2>
+      <Link href="/">Back to home</Link>
+    </section>
+  );
+}
---
+++ src/app/help/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FAQ() {
+  return (
+    <section>
+      <h2>FAQ</h2>
+      <Link href="/">Back to home</Link>
+    </section>
+  );
+}
